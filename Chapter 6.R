1379a217ab519c6593e86a673cdf1fba5b64b081
jjwoolley <jonnywoolley2@gmail.com>
2020-08-25 16:57:52 -0700

    Initializing Chapter 6

---
+++ Chapter 6.R
@@ -0,0 +1,15 @@
+# Follows An Introduction to Statistical Learning
+#   with Applications in R
+# By Gareth James, Daniela Witten, Trevor Hastie and Robert Tibshirani
+
+# http://faculty.marshall.usc.edu/gareth-james/ISL/ISLR%20Seventh%20Printing.pdf
+
+# good notes
+# https://tdg5.github.io/stats-learning-notes/
+
+library("MASS")
+library("ISLR")
+library("tidyverse")
+library("tidymodels")
+library("GGally")
+library("boot")
