3c27f79ed15a2db3b8de517a084448fe6c3269f4
jjwoolley <jonnywoolley2@gmail.com>
2020-08-25 21:37:58 -0700

    Finished up to question 3

--- Chapter 6.R
+++ Chapter 6.R
@@ -13,3 +13,49 @@ library("tidyverse")
 library("tidymodels")
 library("GGally")
 library("boot")
+
+
+
+##### CONCEPTUAL----------------------------------------------------------------
+
+#1a)
+# They could all be tied, but "best subset" will for sure be either tied for the
+#   best or on its own as the best since it includes every combination of variables
+
+#1b)
+# It could be any of the models as best subset could overfit
+
+#1c)
+# (i)   True
+# (ii)  True
+# (iii) False
+# (iv)  False
+# (v)   False
+
+
+#2a)
+# (iii) True, it is less flexible and decreases variance at the price of an 
+#             increase in bias
+
+#2b)
+# (iii) same as in 2a
+
+#2c)
+# (ii) True, this one is more flexible but has less bias as a tradeoff for an
+#             increase in variance
+
+
+#3a)
+# 
+
+#3b)
+#
+
+#3c)
+#
+
+#3d)
+#
+
+#3e)
+#
